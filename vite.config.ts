8e1a8a743afd8453048133198866bfc8fdc8f927
孟庆燕 <1948823325@qq.com>
2025-11-24 16:26:24 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? "/file-compare" : "/",
+  base: process.env.NODE_ENV === 'production' ? "/file-compare/" : "/",
   plugins: [vue()],
   resolve: {
         alias: {
